dc4e6c3fdd455f8744910e4063b3adfdf28c7b02
Juan Lopez <juanlpz713@gmail.com>
2022-09-25 16:13:01 -0700

    Complete RectangleView challenge

--- Recipe List App/View/RectangleView.swift
+++ Recipe List App/View/RectangleView.swift
@@ -9,22 +9,45 @@ import SwiftUI
 
 struct RectangleView: View {
     var body: some View {
-        GeometryReader { geo in
-            VStack {
-                Rectangle()
-                    .frame(width: geo.size.width / 4, height: geo.size.height / 7, alignment: .center)
-                    .onTapGesture {
-                        print("global x: \(geo.frame(in: .global).minX) , y: \(geo.frame(in: .global).minY)")
-                        print("local x: \(geo.frame(in: .local).minX) , y: \(geo.frame(in: .local).minY)")
-                    }
+        VStack(spacing: 0.0) {
+            GeometryReader { geo in
+
                 Rectangle()
                     .foregroundColor(.green)
-                    .frame(width: geo.size.width / 4, height: geo.size.height / 7, alignment: .center)
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
                     .onTapGesture {
                         print("global x: \(geo.frame(in: .global).minX) , y: \(geo.frame(in: .global).minY)")
                         print("local x: \(geo.frame(in: .local).minX) , y: \(geo.frame(in: .local).minY)")
                     }
-            }
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: geo.size.width * 2 / 3, height: geo.size.height / 4, alignment: .center)
+                    .onTapGesture {
+                        print("global x: \(geo.frame(in: .global).minX) , y: \(geo.frame(in: .global).minY)")
+                        print("local x: \(geo.frame(in: .local).minX) , y: \(geo.frame(in: .local).minY)")
+                    }
+                    .position(x: geo.size.width / 2, y: geo.size.height / 2)
+
+            }.ignoresSafeArea()
+
+            GeometryReader { geo in
+                HStack(spacing: 0.0) { Rectangle()
+                    .foregroundColor(.purple)
+                    .frame(width: geo.size.width / 2, height: geo.size.height, alignment: .center)
+                    .onTapGesture {
+                        print("global x: \(geo.frame(in: .global).minX) , y: \(geo.frame(in: .global).minY)")
+                        print("local x: \(geo.frame(in: .local).minX) , y: \(geo.frame(in: .local).minY)")
+                    }
+                    Rectangle()
+                        .foregroundColor(.orange)
+                        .frame(width: geo.size.width / 2, height: geo.size.height, alignment: .center)
+                        .onTapGesture {
+                            print("global x: \(geo.frame(in: .global).minX) , y: \(geo.frame(in: .global).minY)")
+                            print("local x: \(geo.frame(in: .local).minX) , y: \(geo.frame(in: .local).minY)")
+                        }
+                }
+
+            }.ignoresSafeArea()
         }
     }
 }
@@ -34,3 +57,10 @@ struct RectangleView_Previews: PreviewProvider {
         RectangleView()
     }
 }
+
+//  .offset(x: 30, y: 30)
+//  Rectangle()
+//  .foregroundColor(.red)
+//  .frame(width: 100, height: 100, alignment: .center)
+//  .padding([.top, .leading], 30.0)
+//  .offset(x: 30, y: 30)
