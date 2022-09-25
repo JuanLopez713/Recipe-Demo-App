8c5d78c5e233ccb6aeb9e323990bad4e5d5b81f0
Juan Lopez <juanlpz713@gmail.com>
2022-09-25 09:52:30 -0700

    Add button and state control to RecipeTabView

--- Recipe List App/View/RecipeTabView.swift
+++ Recipe List App/View/RecipeTabView.swift
@@ -8,21 +8,28 @@
 import SwiftUI
 
 struct RecipeTabView: View {
+    @State var tabTag = 0;
     var body: some View {
-        TabView {
+        TabView (selection: $tabTag) {
             Text("Featured View")
                 .tabItem {
                     VStack {
                         Image(systemName: "star.fill")
                         Text("Featured")
                     }
+                }.tag(0)
+            Button("Take me to Recipes", action: {tabTag = 2}).tabItem{
+                VStack{
+                    Image(systemName: "star.fill")
+                    Text("Demo")
                 }
+            }.tag(1)
             RecipeContentView().tabItem {
                 VStack {
                     Image(systemName: "list.bullet")
                     Text("List")
                 }
-            }
+            }.tag(2)
         }
     }
 }
