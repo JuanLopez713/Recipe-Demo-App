b1bbb68420eb014fa4ca262250dc452c8863ada3
Juan Lopez <juanlpz713@gmail.com>
2022-09-26 14:37:18 -0700

    Add DetailView to FeaturedView Cards

--- Recipe List App/View/Recipe/RecipeFeaturedView.swift
+++ Recipe List App/View/Recipe/RecipeFeaturedView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RecipeFeaturedView: View {
     @EnvironmentObject var recipeList: RecipeModel
+    @State var isDetailViewShowing = false
     var body: some View {
         VStack(alignment: .leading, spacing: 0.0) {
             Text("Featured Recipes")
@@ -18,21 +19,34 @@ struct RecipeFeaturedView: View {
                 .bold()
             GeometryReader { geo in
                 TabView {
+                    // Loop through each recipe
                     ForEach(0 ..< recipeList.recipes.count, id: \.self) { index in
                         if recipeList.recipes[index].featured == true {
-                            ZStack {
-                                Rectangle().foregroundColor(.white)
+                            Button (action: {
+                                //Show the recipe detail
+                                self.isDetailViewShowing = true
+                            }, label: {
+                                ZStack {
+                                    Rectangle().foregroundColor(.white)
 
-                                VStack(spacing: 0.0) {
-                                    Image(recipeList.recipes[index].image)
-                                        .resizable().clipped()
-                                        .aspectRatio(contentMode: .fill)
-                                    Text(recipeList.recipes[index].name).padding(5)
+                                    VStack(spacing: 0.0) {
+                                        Image(recipeList.recipes[index].image)
+                                            .resizable().clipped()
+                                            .aspectRatio(contentMode: .fill)
+                                        Text(recipeList.recipes[index].name).padding(5)
+                                    }
                                 }
-                            }.frame(width: geo.size.width - 40, height: geo.size.height-100, alignment: .center)
-                                .cornerRadius(20)
-                                .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: 5, y: 5)
+                            })
+                            .sheet(isPresented: $isDetailViewShowing, content: {
+                                //Show recipe detail view
+                                RecipeDetailView(recipe: recipeList.recipes[index])
+                            })
+                            .buttonStyle(PlainButtonStyle())
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                            .cornerRadius(20)
+                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: 5, y: 5)
                         }
+                                    
                     }
                 }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
                     .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
