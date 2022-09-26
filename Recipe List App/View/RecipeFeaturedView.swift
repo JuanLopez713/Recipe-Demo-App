9fa26eaa445025ce94db02bd3eb7bc528e75960b
Juan Lopez <juanlpz713@gmail.com>
2022-09-25 22:31:18 -0700

    Add sliding cards for RecipeFeaturedView

--- Recipe List App/View/RecipeFeaturedView.swift
+++ Recipe List App/View/RecipeFeaturedView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct RecipeFeaturedView: View {
-    @EnvironmentObject var recipeList:RecipeModel
+    @EnvironmentObject var recipeList: RecipeModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 0.0) {
+            Text("Featured Recipes")
+                .padding(.leading, 20.0)
+                .padding(.top, 40.0)
+                .font(.largeTitle)
+                .bold()
+            GeometryReader { geo in
+                TabView {
+                    ForEach(0 ..< recipeList.recipes.count, id: \.self) { index in
+                        if recipeList.recipes[index].featured == true {
+                            ZStack {
+                                Rectangle().foregroundColor(.white)
+
+                                VStack(spacing: 0.0) {
+                                    Image(recipeList.recipes[index].image)
+                                        .resizable().clipped()
+                                        .aspectRatio(contentMode: .fill)
+                                    Text(recipeList.recipes[index].name).padding(5)
+                                }
+                            }.frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                                .cornerRadius(20)
+                                .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: 5, y: 5)
+                        }
+                    }
+                }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                    .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            VStack(alignment: .leading, spacing: 10.0) {
+                Text("Preparation Time").font(.headline)
+                Text("1 hour")
+                Text("Highlight").font(.headline)
+                Text("Healthy, Hearty")
+
+            }.padding([.leading, .bottom], 20.0)
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
